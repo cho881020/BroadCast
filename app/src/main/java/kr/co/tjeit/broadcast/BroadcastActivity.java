19fa8da1a24dacc943a57ce608637d4d854982ce
cho881020 <cho881020@gmail.com>
2017-09-14 17:18:59 +0900

    동적 리시버 구현

---
+++ app/src/main/java/kr/co/tjeit/broadcast/BroadcastActivity.java
@@ -0,0 +1,60 @@
+package kr.co.tjeit.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class BroadcastActivity extends BaseActivity {
+
+    BroadcastReceiver mBroadcastReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+        mBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                String fileName = intent.getStringExtra("fileName");
+
+                Toast.makeText(context, fileName + "이(가) 다운로드 되었습니다.", Toast.LENGTH_SHORT).show();
+
+
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("kr.co.tjeit.broadcast.action.FILE_DOWNLOADED");
+
+        registerReceiver(mBroadcastReceiver, intentFilter);
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mBroadcastReceiver);
+    }
+}
--- app/src/main/java/kr/co/tjeit/broadcast/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/broadcast/MainActivity.java
@@ -2,12 +2,14 @@ package kr.co.tjeit.broadcast;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
 
     private android.widget.Button sendDownloadCompleteBtn;
+    private Button intentBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +22,22 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        intentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, BroadcastActivity.class);
+                startActivity(intent);
+            }
+        });
+
         sendDownloadCompleteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                다운로드 완료 버튼을 누르면
 //                왕좌의게임이 "다운로드 완료"되었다 라고 "방송"
 
-                Intent intent = new Intent();
+                final Intent intent = new Intent();
 //                어떠한 상황인지 알려주는 내용
 
 //                방송 내용 작성 요령
@@ -41,7 +52,14 @@ public class MainActivity extends BaseActivity {
                 intent.putExtra("fileName", "왕좌의게임시즌7E01.avi");
 
 //                실제 방송 진행
-                sendBroadcast(intent);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendBroadcast(intent);
+                    }
+                }, 5000);
+
 
             }
         });
@@ -54,6 +72,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        this.intentBtn = (Button) findViewById(R.id.intentBtn);
         this.sendDownloadCompleteBtn = (Button) findViewById(R.id.sendDownloadCompleteBtn);
 
     }
